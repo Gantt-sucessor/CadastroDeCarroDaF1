d31a0d7edd6ee868b547cedfc1736bd3614e9efb
Gantt-Sucessor <jogoperdi@gmail.com>
2026-01-23 10:56:59 -0400

    first commit

---
+++ src/main/java/dev/FaF1/CadastroDeCarroDaF1/CadastroDeCarroDaF1Application.java
@@ -0,0 +1,13 @@
+package dev.FaF1.CadastroDeCarroDaF1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeCarroDaF1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeCarroDaF1Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/FaF1/CadastroDeCarroDaF1/CadastroDeCarroDaF1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.FaF1.CadastroDeCarroDaF1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeCarroDaF1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
