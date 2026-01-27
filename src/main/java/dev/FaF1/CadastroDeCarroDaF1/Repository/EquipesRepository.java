73286e527259fab1a2fbc040fd4cabd317b59de0
Gantt-sucessor <javarealfan@gmail.com>
2026-01-27 14:34:28 -0400

    feat: Cria a pasta Repository adiciona EquipesRepository

---
+++ src/main/java/dev/FaF1/CadastroDeCarroDaF1/Repository/EquipesRepository.java
@@ -0,0 +1,7 @@
+package dev.FaF1.CadastroDeCarroDaF1.Repository;
+
+import dev.FaF1.CadastroDeCarroDaF1.Model.EquipesModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EquipesRepository extends JpaRepository<EquipesModel, Long> {
+}
